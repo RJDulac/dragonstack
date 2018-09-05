456f9f9d608d9d660ef330fb006448f9956f502c
Ryan Dulac <analyticrye@gmail.com>
2018-09-05 18:25:56 -0400

    improved

--- backend/dragon.js
+++ backend/dragon.js
@@ -1,8 +1,15 @@
+const DEFAULT_PROPERTIES = {
+    nickname: "unnamed",
+    //called to match date on creation of dragon -getter
+    get birthdate() {
+        return new Date()
+    }
+}
 class Dragon {
     //put in curly braces so we don't have to worry about parameter order -non ordered parameters
-    constructor({birthdate, nickname}) {
-        this.birthdate = birthdate;
-        this.nickname = nickname;
+    constructor({ birthdate, nickname } ={}) {
+        this.birthdate = birthdate || DEFAULT_PROPERTIES.birthdate;
+        this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
     }
 }
 
--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,22 @@ const Dragon = require('./dragon.js');
 const fooey = new Dragon({ 
     birthdate: new Date(), 
     nickname: 'fooey'
-});
+}, 3000);
+
+setTimeout(() => {
+    const gooby = new Dragon();
+    console.log('gooby: ', gooby);
+})
 //order doesn't matter!
 const baloo = new Dragon({ 
     nickname: 'baloo', 
     birthdate: new Date()
 });
 
+const mimar = new Dragon();
+
+
 console.log('fooey: ', fooey);
 console.log('baloo: ', baloo);
+
+console.log('mimar: ', mimar);
